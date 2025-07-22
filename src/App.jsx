675eb9d9e6ce27b8ab9ef2b7bc95d6a9bf5b3d92
Daniel Muhia <muhiadan17@gmail.com>
2025-07-22 21:43:59 +0300

    modifies APP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { auth, db, googleProvider } from './firebase/firebase'
 import { signInWithPopup, signOut } from 'firebase/auth'
 import { doc, setDoc, getDoc} from 'firebase/firestore'
+
+
+// Import components
+import Home from './pages/Home'
+import SearchResults from './pages/SearchResults'
+import AuthorPage from './pages/AuthorPage'
+import BookDetail from './pages/BookDetail'
+import ReadingList from './pages/ReadingList'
+import Discover from './pages/Discover'
+import Navbar from './components/Navbar'
+
 
 // Main App component
 function App() {
@@ -51,4 +63,35 @@ function App() {
       console.error('Sign out error:', error)
     }
   }
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar 
+          user={user} 
+          onSignIn={handleGoogleSignIn} 
+          onSignOut={handleSignOut}
+        />
+        
+        <div className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+            <Route path="/author/:authorId" element={<AuthorPage />} />
+            <Route path="/book/:workId" element={<BookDetail user={user} />} />
+            <Route path="/reading-list" element={<ReadingList user={user} />} />
+            <Route path="/discover" element={<Discover />} />
+            <Route path="/discover/:genre" element={<Discover />} />
+          </Routes>
+        </div>
+        
+        <footer className="bg-white fixed bottom-0 border-t py-6 mt-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>© {new Date().getFullYear()} ShelfScope. All book data provided by OpenLibrary.</p>
+          </div>
+        </footer>
+      </div>
+    </Router>
+  )
 }
+
+export default App
